e07e71df399240bc5223aa2b619320f10360882c
Joshua <48004192+Joshngo@users.noreply.github.com>
2022-04-07 20:08:56 -0600

    Add files via upload
    Implemented some code to handle joining rooms, is not functional as I need to know how to merge it into rest of code.

--- src/server/public/client.js
+++ src/server/public/client.js
@@ -19,6 +19,13 @@ socket.on('upload', () => {
     //Code for uploading goes here
 })
 
+socket.on('updateUserList', (userList) => {
+    remakeList(userList);
+})
+
+socket.on('displayNames', (rid) => {
+    showNames(rid)
+})
 
 socket.on('initializationMessage', () => {
     console.log("This is the clienside initialization message");
@@ -29,7 +36,7 @@ socket.on('roomCreateSucess', roomId => {
 }) 
 
 
-// For when a user has joined the session RIGHT after they log in.
+// For when a user has joined the session RIGHT after they log in. (Not really sure if we need this anymore, but I'll keep it here.)
 form.addEventListener('joinSession', (e) => {
     e.preventDefault();
     const input_name = e.target.elements.input.value;
@@ -64,16 +71,40 @@ joinForm.addEventListener('submit', (e) => {
 
 //This function is for when a user wants to create a room, it sends a request to the server to create a room.
 //This function was built assuming that the room name is input through a text field
-form.addEventListener('createRoom', (e) => {
+const createForm = document.getElementById('createForm');
+
+createForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const roomName = e.target.elements.input.value;
+    const userName = createForm.elements['userName']
+    const roomName = createForm.elements['roomName'];
     roomName.trim();
+    username.trim();
 
     //Send request to server
     socket.emit('createRoom', roomName);
+
     e.target.elements.input.value=''; //Probably not needed but just in case.
 
 })
 
+// here we change the html DOM to remake the list on the sidebar when a socket joins or disconnects.
+// The param: Users should be an array of users that are in a certain room. Changing the the DOM for these users is the plan.
+function remakeList(users) {
+    const list = document.getElementById('sidebarList'); 
 
+    while(list.hasChildNodes()) {
+        list.removeChild(list.firstChild);
+    }
 
+    for(let x in users) {
+        let username = (users[x].username);
+        const new_user = document.createElement('li');
+        new_user.innerHTML = username;
+        document.getElementById('userList').appendChild(new_user);
+    }
+}   
+
+function showNames(rid) {
+    const display = document.getElementById('room-id');
+    display.innerHTML="Room ID: " + roomId;
+}
